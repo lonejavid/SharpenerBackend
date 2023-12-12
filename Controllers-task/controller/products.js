914fea8552edaacbf6c67f05d8dac823d07761ca
lonejavid <lonejavid0739@gmail.com>
2023-12-12 14:05:10 +0530

    models added into shop application

--- Controllers-task/controller/products.js
+++ Controllers-task/controller/products.js
@@ -1,5 +1,5 @@
 
-const products = [];
+const Product=require('../models/product') 
 
 exports.getAddProduct=(req, res, next) => {
     res.render('add-product', {
@@ -13,13 +13,17 @@ exports.getAddProduct=(req, res, next) => {
 
 
   exports.postAddProduct=(req, res, next) => {
-    products.push({ title: req.body.title });
+    //products.push({ title: req.body.title });
+    const pro= new Product(req.body.title);
+    
+    pro.save()
     res.redirect('/');
   }
-  exports.products = products;
+  //exports.products = products;
 
  exports.getProducts= (req, res, next) => {
-    
+    const products = Product.fetchAll();
+  
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
