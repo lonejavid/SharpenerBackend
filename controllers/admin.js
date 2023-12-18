c7b0159c0ff85ad42fc1eed5598a456309a1c7cf
lonejavid <lonejavid0739@gmail.com>
2023-12-18 20:39:24 +0530

    edit and delete buttons wrok properly

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,24 +1,79 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    edit: false
+    
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
+  console.log("called")
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
+  const price = parseInt(req.body.price);
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode=req.query.edit;
+  
+  
+  if(!editMode){
+    return  res.redirect('/')
+  }
+  const prodId=req.params.productId
+  
+  Product.findByPk(prodId,product=>{
+    if(!product){
+      return res.redirect('/');
+    }
+   //console.log(prodId)
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      edit: editMode,
+      product: product
+      
+    });
+    
+  })
+ 
+};
+
+exports.postEditProduct=(req,res,next)=>{
+  const prodId=req.body.productId;
+  const updatedTitle=req.body.title;
+  const updatedPrice=req.body.price;
+  const updatedImageUrl=req.body.imageUrl;
+  const updatedDesc=req.body.description;
+  const updatedProduct= new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+    );
+    console.log(updatedProduct)
+    updatedProduct.save();
+    res.redirect('/admin/products')
+
+
+}
+exports.postDelete=(req,res,next)=>{
+  const productId = req.body.productId;
+  Product.deleteById(productId)
+  res.redirect('/');
+  
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
--- routes/shop.js
+++ routes/shop.js
@@ -18,6 +18,9 @@ router.post('/cart',shopController.postCart)
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart',shopController.postCart)
+
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
